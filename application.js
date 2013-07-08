73efb7c427d30399b41fdec8046eef111718e058
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-07-07 18:16:36 -0700

    completed

--- application.js
+++ application.js
@@ -1,32 +1,45 @@
 $(document).ready(function() {
   var todoTemplate = $.trim($('#todo_template').html());
-  // var fullList = $('.todo_list');
+  var fullList = $('.todo_list');
+  
+
   function bindEvents() {
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
     $('.toolbox').on('click','.add',function(e){
       e.preventDefault();
-      var inputValue = $('input').val();
-      addTodo(buildTodo(inputValue));
+      var task = $('input').val();
+      addTodo(buildTodo(task));
     });
 
-    $('.todo_list').on('click','.delete', function(e) {
+    $(fullList).on('click','.delete', function(e) {
       e.preventDefault();
-      $(this).closest('div.todo').remove();
+      var $this = $(this).closest('.todo');
+      removeTodo($this);
     });
 
-    $('.todo_list').on('click','.complete', function(e) {
+    $(fullList).on('click','.complete', function(e) {
       e.preventDefault();
-      $(this).closest('div.todo').toggleClass("complete");
+      var $this = $(this).closest('.todo');
+      completeTodo($this);
     });
   }
 
   //Create functions to add, remove and complete todos
   function addTodo(todo) {
-    $('.todo_list').append(todo);
+    $(fullList).append(todo);
   }
 
-  function buildTodo(todoName) {
+  function removeTodo(todo) {
+   todo.remove();
+ }
+
+ function completeTodo(todo) {
+  todo.toggleClass("complete");
+}
+
+
+function buildTodo(todoName) {
     // Creates an jQueryDOMElement from the todoTemplate.
     var $todo = $(todoTemplate);
     // Modifies it's text to use the passed in todoName.
@@ -34,7 +47,7 @@ $(document).ready(function() {
     // Returns the jQueryDOMElement to be used elsewhere.
     return $todo;
   }
- 
+
 
   bindEvents();
 });
